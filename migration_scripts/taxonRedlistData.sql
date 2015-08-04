5e542a82bd1acbdecd116b583bf880e691c172f3
Thomas Stjernegaard Jeppesen <t.stjernegaard@snm.ku.dk>
2015-08-04 13:27:27 +0200

    Main part of red list UI done

--- migration_scripts/taxonRedlistData.sql
+++ migration_scripts/taxonRedlistData.sql
@@ -1,4 +1,7 @@
 CREATE TABLE IF NOT EXISTS `TaxonRedListData` (
+	`_id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	`createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	 `updatedAt` datetime DEFAULT NULL,
 	`taxon_id` INT(11) NOT NULL,
 	 `Artstilbagegang` INT(11),
     `Status` ENUM('LC', 'NT', 'VU', 'EN', 'CR','RE', 'DD', 'NA', 'NE'),
@@ -8,7 +11,7 @@ CREATE TABLE IF NOT EXISTS `TaxonRedListData` (
 	`StatusTal` INT(11),
     `Roedlisteansvarlig` VARCHAR(255),
     `roedlisteBestandsstoerrelse` INT(11),
-    `roedlisteBestandsstoerrelseÅr` VARCHAR(11),
+    `roedlisteBestandsstoerrelseAar` VARCHAR(11),
     `roedlisteBestandsstoerrelseBem` text,
     `roedlisteBestandsudvikling` text,
     `roedlisteGenerationsinterval` INT(4),
@@ -29,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `TaxonRedListData` (
     `Litteratur` text,
     `Litteratur_DMU` VARCHAR(255),
 	`Kriterier` VARCHAR(255),
-	PRIMARY KEY (taxon_id, year)
+	UNIQUE (taxon_id, year)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 UPDATE TaxonBase SET Artstilbagegang = NULL where Artstilbagegang = "";
@@ -49,6 +52,24 @@ UPDATE TaxonBase SET roedlisteBestandsstoerrelse = "38" where roedlisteBestandss
 UPDATE TaxonBase SET roedlisteID = NULL where roedlisteID = "";
 UPDATE TaxonBase SET roedlisteBestandsstoerrelse = NULL where roedlisteBestandsstoerrelse = "";
 
+-- missing taxa table
+UPDATE TaxonBase_missing_taxa SET Artstilbagegang = NULL where Artstilbagegang = "";
+UPDATE TaxonBase_missing_taxa SET roedlisteBestandsstoerrelse = NULL where roedlisteBestandsstoerrelse = "";
+
+UPDATE TaxonBase_missing_taxa SET roedlisteGenerationsinterval = NULL where roedlisteGenerationsinterval = "";
+UPDATE TaxonBase_missing_taxa SET roedlisteGenerationstid = NULL where roedlisteGenerationstid = "";
+UPDATE TaxonBase_missing_taxa SET roedlisteUdbredelsesareal = NULL where roedlisteUdbredelsesareal = "";
+UPDATE TaxonBase_missing_taxa SET Artstilbagegang = 0 where Artstilbagegang = "Der er ikke påvist nogen tilbagegang for  arten.";
+UPDATE TaxonBase_missing_taxa SET Artstilbagegang = 0 where Artstilbagegang = "00";
+UPDATE TaxonBase_missing_taxa SET Artstilbagegang = 10 where Artstilbagegang = "<10";
+UPDATE TaxonBase_missing_taxa SET Artstilbagegang = NULL where Artstilbagegang = "";
+
+
+UPDATE TaxonBase_missing_taxa SET roedlisteBestandsstoerrelse = "38" where roedlisteBestandsstoerrelse = "37,5";
+
+UPDATE TaxonBase_missing_taxa SET roedlisteID = NULL where roedlisteID = "";
+UPDATE TaxonBase_missing_taxa SET roedlisteBestandsstoerrelse = NULL where roedlisteBestandsstoerrelse = "";
+
 -- 2009
 
 INSERT INTO TaxonRedListData (
@@ -60,7 +81,7 @@ INSERT INTO TaxonRedListData (
 	`StatusTal` ,
     `Roedlisteansvarlig` ,
     `roedlisteBestandsstoerrelse` ,
-    `roedlisteBestandsstoerrelseÅr` ,
+    `roedlisteBestandsstoerrelseAar` ,
     `roedlisteBestandsstoerrelseBem` ,
     `roedlisteBestandsudvikling` ,
     `roedlisteGenerationsinterval` ,
@@ -110,6 +131,66 @@ INSERT INTO TaxonRedListData (
     `Litteratur` ,
     `Litteratur_DMU` ,
 	`Kriterier` FROM TaxonBase WHERE roedliste_2009 IS NOT NULL AND roedliste_2009 <> "" AND roedlisteGruppe <> "" AND StatusDK <> "";
+	
+	INSERT INTO TaxonRedListData (
+		`taxon_id`,
+		 `Artstilbagegang`,
+		`VerbatimStatus`,
+		`year` ,
+	    `StatusDK` ,
+		`StatusTal` ,
+	    `Roedlisteansvarlig` ,
+	    `roedlisteBestandsstoerrelse` ,
+	    `roedlisteBestandsstoerrelseAar` ,
+	    `roedlisteBestandsstoerrelseBem` ,
+	    `roedlisteBestandsudvikling` ,
+	    `roedlisteGenerationsinterval` ,
+	    `roedlisteGenerationstid`,
+	    `roedlisteGruppe` ,
+	    `roedlisteID` ,
+	    `roedlisteIndivider`,
+	    `roedlisteLevesteder`,
+	    `roedlisteNationalStatus` ,
+	    `roedlisteNationalStatusClean` ,
+	    `roedlisteNegativePaavirknBem` ,
+	    `roedlisteNegativePaavirkninger` ,
+	    `roedlisteNomenkaltur` ,
+	    `roedlisteUdbredelsesareal` ,
+	    `Roedlistevurdering` ,
+		`NationalRoedlistekatogeri` ,
+		`Udbredelse`,
+	    `Litteratur` ,
+	    `Litteratur_DMU` ,
+		`Kriterier` 
+	) SELECT `DkIndexNumber`,
+		 `Artstilbagegang`,
+		`roedliste_2009`,
+		2009 ,
+	    `StatusDK` ,
+		`StatusTal` ,
+	    `Roedlisteansvarlig` ,
+	    `roedlisteBestandsstoerrelse` ,
+	    `roedlisteBestandsstoerrelseÅr` ,
+	    `roedlisteBestandsstoerrelseBem` ,
+	    `roedlisteBestandsudvikling` ,
+	    `roedlisteGenerationsinterval` ,
+	    `roedlisteGenerationstid` ,
+	    `roedlisteGruppe` ,
+	    `roedlisteID` ,
+	    `roedlisteIndivider`,
+	    `roedlisteLevesteder`,
+	    `roedlisteNationalStatus` ,
+	    `roedlisteNationalStatusClean` ,
+	    `roedlisteNegativePaavirknBem` ,
+	    `roedlisteNegativePaavirkninger` ,
+	    `roedlisteNomenkaltur` ,
+	    `roedlisteUdbredelsesareal` ,
+	    `Roedlistevurdering` ,
+		`NationalRoedlistekatogeri` ,
+		`Udbredelse`,
+	    `Litteratur` ,
+	    `Litteratur_DMU` ,
+		`Kriterier` FROM TaxonBase_missing_taxa WHERE roedliste_2009 IS NOT NULL AND roedliste_2009 <> "" AND roedlisteGruppe <> "" AND StatusDK <> "";
 
 
 	-- 2005
@@ -148,4 +229,14 @@ INSERT INTO TaxonRedListData (
 				1990 FROM TaxonBase WHERE Gl_status IS NOT NULL AND Gl_status <> "" AND roedlisteGruppe <> "" AND StatusDK <> "";
 
 
+UPDATE TaxonRedListData SET Status = "LC" WHERE VerbatimStatus LIKE "%LC%";
+UPDATE TaxonRedListData SET Status = "NT" WHERE VerbatimStatus LIKE "%NT%";
+UPDATE TaxonRedListData SET Status = "VU" WHERE VerbatimStatus LIKE "%VU%";
+UPDATE TaxonRedListData SET Status = "EN" WHERE VerbatimStatus LIKE "%EN%";
+UPDATE TaxonRedListData SET Status = "CR" WHERE VerbatimStatus LIKE "%CR%";
+UPDATE TaxonRedListData SET Status = "RE" WHERE VerbatimStatus LIKE "%RE%";
+UPDATE TaxonRedListData SET Status = "DD" WHERE VerbatimStatus LIKE "%DD%";
+UPDATE TaxonRedListData SET Status = "NA" WHERE VerbatimStatus LIKE "%NA%";
+UPDATE TaxonRedListData SET Status = "NE" WHERE VerbatimStatus LIKE "%NE%";
 
+Flere ? hvad med f.eks."DK:  R", "DK (E)", "DK (Ex)", "(V)" ??
