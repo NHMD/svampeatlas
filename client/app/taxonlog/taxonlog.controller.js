44a3eb04b265f15355647b77ebd53482dc1515ce
Thomas Stjernegaard Jeppesen <t.stjernegaard@snm.ku.dk>
2015-09-15 14:40:28 +0200

    Closes #36

--- client/app/taxonlog/taxonlog.controller.js
+++ client/app/taxonlog/taxonlog.controller.js
@@ -15,12 +15,12 @@ angular.module('svampeatlasApp')
 
 	      var offset = pagination.start || 0;     // This is NOT the page number, but the index of item in the list that you want to use to display the table.
 	      var limit = pagination.number || 50;  // Number of entries showed per page.
-		  var where = (tableState.search.predicateObject) ? _.mapValues(tableState.search.predicateObject, function(value, key){
-			  
-			  return {like : value += "%"};
-		  }) : undefined;
 		  
-		 
+		  
+		  var where = (tableState.search.predicateObject) ? _.mapValues(_.omit(tableState.search.predicateObject, 'User', 'Taxon'), function(value, key){
+			  
+			  return {like : "%"+ value + "%"};
+		  }) : undefined;
 		  
 		  var order = tableState.sort.predicate;
 		  if(tableState.sort.reverse) {
@@ -31,6 +31,27 @@ angular.module('svampeatlasApp')
 		  if(where) {
 			  query['where']= JSON.stringify(where)
 		  };
+		  
+		  var userWhere = (tableState.search.predicateObject && tableState.search.predicateObject.User) ? _.mapValues(tableState.search.predicateObject.User, function(value, key){
+			  
+			  return {like : value + "%"};
+		  }) : undefined;
+		  
+		  var taxonWhere = (tableState.search.predicateObject && tableState.search.predicateObject.Taxon) ? _.mapValues(tableState.search.predicateObject.Taxon, function(value, key){
+			  
+			  return {like : "%" +value + "%"};
+		  }) : undefined;
+		   
+  		query.include = JSON.stringify([{
+				model: "User",
+				as: "User",
+			where: JSON.stringify(userWhere)
+			}, {
+				model: "Taxon",
+				as: "Taxon",
+			where: JSON.stringify(taxonWhere)
+			}]);
+		  
 	      TaxonLog.query(query, function (result, headers) {
 	        $scope.displayed = result;
 	      	 tableState.pagination.numberOfPages = Math.ceil(headers('count') / limit);//set the number of pages so the pagination can update
