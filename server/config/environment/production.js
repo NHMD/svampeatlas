2748650631a6cfb1ed5593afb1b1a2c40362fa71
Thomas Stjernegaard Jeppesen <t.stjernegaard@snm.ku.dk>
2015-10-14 20:51:41 +0200

    Updated nodejs version in packagage.json

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -12,6 +12,15 @@ module.exports = {
   port:     process.env.OPENSHIFT_NODEJS_PORT ||
             process.env.PORT ||
             8080,
+  
+   plutof: {
+   	client_id: 'fJ43ypTmHBhsNjr6NoxqJ3MN7FbSmnD3anNp5Q16',
+   	client_secret: 'sukZIxGIgusbE67TkURKmjvHcixd5l1uEqdXlZ9kWEHD7WIV0xmnH01BtOrHXmYQGQk63Tdxd2xOVXQeP00rXb7ZM0c5yfagm97vLoPZEqROeTDg8li8waSNc8tb2cR8',
+   	grant_type: 'password',
+   	username: 'svampeatlas',
+   	password: 'selandicus031003',
+   	scope: 'read'
+   },
 
   // MongoDB connection options
   mongo: {
