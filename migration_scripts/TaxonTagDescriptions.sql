6433aacb6a8304c0e565d0bf372cb5bae5bb017f
Thomas Stjernegaard Jeppesen <t.stjernegaard@snm.ku.dk>
2015-11-11 09:54:59 +0100

    Closes #62

--- migration_scripts/TaxonTagDescriptions.sql
+++ migration_scripts/TaxonTagDescriptions.sql
@@ -1,6 +1,6 @@
 CREATE TABLE TaxonTagDescriptions (
 _id INT(11) NOT NULL PRIMARY KEY,
-createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 tagname VARCHAR(255),
 tagowner INT(11) NOT NULL,
 FOREIGN KEY (tagowner) REFERENCES Users (_id)
--- migration_scripts/TaxonTags.sql
+++ migration_scripts/TaxonTags.sql
@@ -1,7 +1,7 @@
 CREATE TABLE TaxonTags (
 tag_id INT(11) NOT NULL,
 taxon_id INT(11) NOT NULL,
-createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY (tag_id, taxon_id),
 FOREIGN KEY (tag_id) REFERENCES TaxonTagDescriptions (_id) ON DELETE CASCADE,
 FOREIGN KEY (taxon_id) REFERENCES Taxon (_id) ON DELETE CASCADE
