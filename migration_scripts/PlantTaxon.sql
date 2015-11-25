7fc2c5519c98f96b4ebb6619c847f9cb113f35bf
Thomas Stjernegaard Jeppesen <t.stjernegaard@snm.ku.dk>
2015-11-25 13:33:39 +0100

    Migration of associatedOrganism, splitting into relational table and linking to GBIF taxonomy

--- migration_scripts/PlantTaxon.sql
+++ migration_scripts/PlantTaxon.sql
@@ -7,22 +7,49 @@ Ectomycorrhizal VARCHAR(11),
 Genus VARCHAR(11),
 LatinName VARCHAR(255) NOT NULL,
 LatinCode VARCHAR(11) 
-WoodySubstrate VARCHAR(11)
+WoodySubstrate VARCHAR(11),
+accepted_id INT(11),
+parent_id INT(11)
 ) ENGINE = InnoDB DEFAULT CHARSET=UTF8;
 
--- direct match on DKname
-UPDATE Observation o, PlantTaxon p, Fungi f SET o.primaryassociatedorganism_id = p._id where o._id=f.AtlasLNR AND f.associatedOrganism= p.DKname;
+UPDATE PlantTaxon set Genus = 1 where Genus = "X";
+UPDATE PlantTaxon set Genus = 0 where Genus = "";
+alter table PlantTaxon modify Genus tinyint(1);
+-- Find all duplicate latin names (45)
+SELECT * FROM PlantTaxon p, (select LatinName, COUNT(*) FROM PlantTaxon GROUP by LatinName HAVING  COUNT(*) > 1) a where p.LatinName = a.LatinName ORDER BY p.LatinName;
+
+-- Synonymize where we have several dk names for plants
+UPDATE PlantTaxon p, PlantTaxon p1 SET p1.accepted_id = p._id WHERE p.LatinName=p1.LatinName AND p._id < p1._id;
+
+-- Self synonymize
+UPDATE PlantTaxon set accepted_id = _id WHERE accepted_id IS NULL;
+
+-- Map Species to genera
+UPDATE PlantTaxon p, PlantTaxon p1 SET p1.parent_id = p._id WHERE p1.LatinName LIKE CONCAT(p.LatinName, "%") AND p.Genus = 1 AND p1.Genus =0;
+
+
+
 -- direct match on concatted DKname and latin name
-UPDATE Observation o, PlantTaxon p, Fungi f SET o.primaryassociatedorganism_id = p._id where o._id=f.AtlasLNR AND f.associatedOrganism= p.DKandLatinName;
+UPDATE Observation o, PlantTaxon p, Fungi f SET o.primaryassociatedorganism_id = p.accepted_id where o._id=f.AtlasLNR AND f.associatedOrganism= p.DKandLatinName;
+
 -- direct match on latin name
-UPDATE Observation o, PlantTaxon p, Fungi f SET o.primaryassociatedorganism_id = p._id where o._id=f.AtlasLNR AND f.associatedOrganism= p.LatinName;
+UPDATE Observation o, PlantTaxon p, Fungi f SET o.primaryassociatedorganism_id = p.accepted_id where o._id=f.AtlasLNR AND f.associatedOrganism= p.LatinName;
+-- direct match on DKname to Genus
+UPDATE Observation o, PlantTaxon p, Fungi f SET o.primaryassociatedorganism_id = p.accepted_id where o._id=f.AtlasLNR AND f.associatedOrganism= p.DKname AND p.Genus =1;
+
+-- direct match on DKname to Species
+UPDATE Observation o, PlantTaxon p, Fungi f SET o.primaryassociatedorganism_id = p.accepted_id where o._id=f.AtlasLNR AND f.associatedOrganism= p.DKname AND p.Genus =0 AND o.primaryassociatedorganism_id IS NULL;
+
+
+-- match on first taxon if multiple, dk name and latin
+UPDATE Observation o, PlantTaxon p, Fungi f SET o.primaryassociatedorganism_id = p.accepted_id where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(f.associatedOrganism, ", ", 1) = p.DKandLatinName;
+-- match on first taxon if multiple, latin name
+UPDATE Observation o, PlantTaxon p, Fungi f SET o.primaryassociatedorganism_id = p.accepted_id where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(f.associatedOrganism, ", ", 1) = p.LatinName;
 
 -- match on first taxon if multiple, dk name
-UPDATE Observation o, PlantTaxon p, Fungi f SET o.primaryassociatedorganism_id = p._id where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(f.associatedOrganism, ", ", 1) = p.DKname;
--- match on first taxon if multiple, dk name and latin
-UPDATE Observation o, PlantTaxon p, Fungi f SET o.primaryassociatedorganism_id = p._id where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(f.associatedOrganism, ", ", 1) = p.DKandLatinName;
--- match on first taxon if multiple, latin name
-UPDATE Observation o, PlantTaxon p, Fungi f SET o.primaryassociatedorganism_id = p._id where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(f.associatedOrganism, ", ", 1) = p.LatinName;
+UPDATE Observation o, PlantTaxon p, Fungi f SET o.primaryassociatedorganism_id = p.accepted_id where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(f.associatedOrganism, ", ", 1) = p.DKname AND p.Genus =1;
+
+UPDATE Observation o, PlantTaxon p, Fungi f SET o.primaryassociatedorganism_id = p.accepted_id where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(f.associatedOrganism, ", ", 1) = p.DKname AND p.Genus =0 AND o.primaryassociatedorganism_id IS NULL;
 
 
 
@@ -38,13 +65,21 @@ CREATE TABLE ObservationPlantTaxon (
 INSERT INTO ObservationPlantTaxon SELECT primaryassociatedorganism_id, _id FROM Observation WHERE primaryassociatedorganism_id IS NOT NULL;
 
 --insert all secondary taxa 
-INSERT INTO ObservationPlantTaxon SELECT p._id, o._id FROM Observation o, PlantTaxon p, Fungi f  where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(SUBSTRING_INDEX(f.associatedOrganism, ", ", -1), ", ", 2) = p.DKname;
-INSERT INTO ObservationPlantTaxon SELECT p._id, o._id FROM Observation o, PlantTaxon p, Fungi f  where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(SUBSTRING_INDEX(f.associatedOrganism, ", ", -1), ", ", 2) = p.DKandLatinName;
-INSERT INTO ObservationPlantTaxon SELECT p._id, o._id FROM Observation o, PlantTaxon p, Fungi f  where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(SUBSTRING_INDEX(f.associatedOrganism, ", ", -1), ", ", 2) = p.LatinName;
+INSERT INTO ObservationPlantTaxon SELECT p.accepted_id, o._id FROM Observation o, PlantTaxon p, Fungi f  where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(f.associatedOrganism, ", ", -1) = p.DKname AND p.Genus=1 ON DUPLICATE KEY UPDATE planttaxon_id=p.accepted_id, observation_id =o._id;
+INSERT INTO ObservationPlantTaxon SELECT p.accepted_id, o._id FROM Observation o, PlantTaxon p, Fungi f  where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(f.associatedOrganism, ", ", -1) = p.DKandLatinName ON DUPLICATE KEY UPDATE planttaxon_id=p.accepted_id, observation_id =o._id;
+INSERT INTO ObservationPlantTaxon SELECT p.accepted_id, o._id FROM Observation o, PlantTaxon p, Fungi f  where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(f.associatedOrganism, ", ", -1) = p.LatinName ON DUPLICATE KEY UPDATE planttaxon_id=p.accepted_id, observation_id =o._id;
 
 -- insert third taxon if exists
-INSERT INTO ObservationPlantTaxon SELECT p._id, o._id FROM Observation o, PlantTaxon p, Fungi f  where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(SUBSTRING_INDEX(f.associatedOrganism, ", ", -1), ", ", 3) = p.DKname;
-INSERT INTO ObservationPlantTaxon SELECT p._id, o._id FROM Observation o, PlantTaxon p, Fungi f  where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(SUBSTRING_INDEX(f.associatedOrganism, ", ", -1), ", ", 3) = p.DKandLatinName;
-INSERT INTO ObservationPlantTaxon SELECT p._id, o._id FROM Observation o, PlantTaxon p, Fungi f  where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(SUBSTRING_INDEX(f.associatedOrganism, ", ", -1), ", ", 3) = p.LatinName;
+INSERT INTO ObservationPlantTaxon SELECT p.accepted_id, o._id FROM Observation o, PlantTaxon p, Fungi f  where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(SUBSTRING_INDEX(f.associatedOrganism, ", ", -2), ", ", 1) = p.DKname AND p.Genus=1 ON DUPLICATE KEY UPDATE planttaxon_id=p._id, observation_id =o._id;
+INSERT INTO ObservationPlantTaxon SELECT p.accepted_id, o._id FROM Observation o, PlantTaxon p, Fungi f  where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(SUBSTRING_INDEX(f.associatedOrganism, ", ", -2), ", ", 1) = p.DKandLatinName ON DUPLICATE KEY UPDATE planttaxon_id=p._id, observation_id =o._id;
+INSERT INTO ObservationPlantTaxon SELECT p.accepted_id, o._id FROM Observation o, PlantTaxon p, Fungi f  where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(SUBSTRING_INDEX(f.associatedOrganism, ", ", -2), ", ", 1) = p.LatinName ON DUPLICATE KEY UPDATE planttaxon_id=p._id, observation_id =o._id;
 
+-- insert fourth taxon if exists
+INSERT INTO ObservationPlantTaxon SELECT p.accepted_id, o._id FROM Observation o, PlantTaxon p, Fungi f  where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(SUBSTRING_INDEX(f.associatedOrganism, ", ", -3), ", ", 1) = p.DKname AND p.Genus=1 ON DUPLICATE KEY UPDATE planttaxon_id=p._id, observation_id =o._id;
+INSERT INTO ObservationPlantTaxon SELECT p.accepted_id, o._id FROM Observation o, PlantTaxon p, Fungi f  where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(SUBSTRING_INDEX(f.associatedOrganism, ", ", -3), ", ", 1) = p.DKandLatinName ON DUPLICATE KEY UPDATE planttaxon_id=p._id, observation_id =o._id;
+INSERT INTO ObservationPlantTaxon SELECT p.accepted_id, o._id FROM Observation o, PlantTaxon p, Fungi f  where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(SUBSTRING_INDEX(f.associatedOrganism, ", ", -3), ", ", 1) = p.LatinName ON DUPLICATE KEY UPDATE planttaxon_id=p._id, observation_id =o._id;
+-- insert fifth taxon if exists
+INSERT INTO ObservationPlantTaxon SELECT p.accepted_id, o._id FROM Observation o, PlantTaxon p, Fungi f  where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(SUBSTRING_INDEX(f.associatedOrganism, ", ", -4), ", ", 1) = p.DKname AND p.Genus=1 ON DUPLICATE KEY UPDATE planttaxon_id=p._id, observation_id =o._id;
+INSERT INTO ObservationPlantTaxon SELECT p.accepted_id, o._id FROM Observation o, PlantTaxon p, Fungi f  where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(SUBSTRING_INDEX(f.associatedOrganism, ", ", -4), ", ", 1) = p.DKandLatinName ON DUPLICATE KEY UPDATE planttaxon_id=p._id, observation_id =o._id;
+INSERT INTO ObservationPlantTaxon SELECT p.accepted_id, o._id FROM Observation o, PlantTaxon p, Fungi f  where f.associatedOrganism LIKE "%,%" AND o._id=f.AtlasLNR AND SUBSTRING_INDEX(SUBSTRING_INDEX(f.associatedOrganism, ", ", -4), ", ", 1) = p.LatinName ON DUPLICATE KEY UPDATE planttaxon_id=p._id, observation_id =o._id;
  
