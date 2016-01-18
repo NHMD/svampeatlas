8b994ffe58dd1b58c979fdbd2657a5d9788c9f9f
Thomas Stjernegaard Jeppesen <t.stjernegaard@snm.ku.dk>
2016-01-18 13:37:03 +0100

    Closes #78 Closes #81

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -50,7 +50,8 @@ var all = {
     clientID:     process.env.GOOGLE_ID || 'id',
     clientSecret: process.env.GOOGLE_SECRET || 'secret',
     callbackURL:  (process.env.DOMAIN || '') + '/auth/google/callback'
-  }
+  },
+  geonames: 'svampeatlas'
 };
 
 // Export the config object based on the NODE_ENV
