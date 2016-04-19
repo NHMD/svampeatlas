6fe2eba61d9b18fc912dde3105524005578e97cb
Thomas Stjernegaard Jeppesen <t.stjernegaard@snm.ku.dk>
2016-04-19 14:21:13 +0200

    Fixes danish letters in capitalize filter

--- client/app/observationmodal/observationModal.service.js
+++ client/app/observationmodal/observationModal.service.js
@@ -249,12 +249,9 @@ angular.module('svampeatlasApp')
 		}
 
 )
-	.
-filter('capitalize', function() {
-	return function(input, all) {
-		var reg = (all) ? /([^\W_]+[^\s-]*) */g : /([^\W_]+[^\s-]*)/;
-		return ( !! input) ? input.replace(reg, function(txt) {
-			return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-		}) : '';
-	}
+	
+.filter('capitalize', function() {
+    return function(input) {
+      return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+    }
 });
