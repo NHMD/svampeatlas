042907f9492782d8bcf7991a732921d26e690c43
Thomas Stjernegaard Jeppesen <t.stjernegaard@snm.ku.dk>
2016-08-19 12:24:08 +0200

    minor changes

--- client/app/demos/demos.controller.js
+++ client/app/demos/demos.controller.js
@@ -9,7 +9,15 @@ angular.module('svampeatlasApp')
 		  { title: 'Hvordan man lægger fund i svampeatlas', url: 'http://quick.as/xw4zcbzvx', icon: 'ondemand_video' },
 		  { title: 'Indtastning af udenlandske fund i Svampeatlas', url: 'http://quick.as/YznXs8nOz' , icon: 'ondemand_video'},
 		  { title: 'Søgning i svampeatlas', url: 'https://www.facebook.com/groups/svampeatlas/permalink/10154445124923522/' , icon: 'facebook'},
-		  { title: 'Rapportører og findere', url: 'https://www.facebook.com/groups/svampeatlas/permalink/10154445112068522/' , icon: 'facebook'}
+		  { title: 'Rapportører og findere', url: 'https://www.facebook.com/groups/svampeatlas/permalink/10154445112068522/' , icon: 'facebook'},
+		  { title: 'Angivelse af finder som ikke findes i systemet', url: 'http://quick.as/o0jrtdbzk' , icon: 'ondemand_video'},
+		  { title: 'Udvidet søgning efter værtsorganisme', url: ' http://quick.as/mrzqf6gq9' , icon: 'ondemand_video'}
+		  
+		 
+		  
+		  
+		  
+		  
 		  
 		
 	  ]
