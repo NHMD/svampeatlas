aa6cf04efcef6fb7d8acfef732a851dfbece92f5
Thomas Stjernegaard Jeppesen <t.stjernegaard@snm.ku.dk>
2016-12-05 10:08:52 +0100

    closes #271 closes #272 closes #273 closes #274

--- migration_scripts/darwinCoreView2.sql
+++ migration_scripts/darwinCoreView2.sql
@@ -1,18 +1,17 @@
 CREATE VIEW `darwincoreview` AS 
 select 
-CONCAT('http://svampe.databasen.org/observations/', o._id) AS `id`,
+o._id AS `id`,
 GREATEST(o.updatedAt, COALESCE(d.updatedAt,0), COALESCE(t.updatedAt,0),  COALESCE(ta.updatedAt,0)) as `dcterms:modified`,
 'HumanObservation' AS basisOfRecord,
 'da' AS `dcterms:language`,
 'https://creativecommons.org/licenses/by-nc/4.0/' AS `dcterms:license`,
 CONCAT('Danish Mycological Society (',DATE_FORMAT(NOW(),'%Y-%m-%d'), '). Fungal records database (http://svampe.databasen.org), contributed by Frøslev, T., Heilmann-Clausen, J., Jeppesen, T.S., Lange, C., Læssøe, T., Petersen, J.H., Søchting, U., Vesterholt, J.' )    AS `dcterms:bibliographicCitation`,
-
 CONCAT('http://svampe.databasen.org/observations/', o._id) AS `occurrenceID`,
-
 CONCAT('DMS-',o._id) AS `catalogNumber`,
 o.fieldNumber AS fieldNumber,
 o.fieldNumber AS otherCatalogNumbers,
 if((o.substrate_id IS NULL),NULL,concat('{"Substrate":"',`s`.`name_uk`,'"}')) AS `dynamicProperties`,
+
 ptx.DKandLatinName AS `associatedTaxa`,
 GROUP_CONCAT(CONCAT('http://svampe.databasen.org/uploads/', oi.name, '.JPG') SEPARATOR ' | ')  AS `associatedMedia`,
 u.name AS `recordedBy`,
@@ -33,6 +32,8 @@ CONCAT(ta.FullName, " ", ta.Author) AS `scientificName`,
 dkn.vernacularname_dk AS `vernacularName`,
 CONCAT('urn:lsid:indexfungorum.org:names:', ta.FunIndexNumber) AS `scientificNameID`,
 dtm.name AS identifiedBy 
+
+
 FROM Observation o LEFT JOIN Locality l ON o.locality_id=l._id LEFT JOIN Users u ON o.primaryuser_id= u._id LEFT JOIN GeoNames gn ON o.geonameId = gn.geonameId LEFT JOIN Determination d ON o.primarydetermination_id= d._id LEFT JOIN Substrate s ON o.substrate_id=s._id LEFT JOIN VegetationType v ON o.vegetationtype_id = v._id LEFT JOIN PlantTaxon ptx ON o.primaryassociatedorganism_id=ptx._id
 LEFT JOIN Taxon t ON d.taxon_id=t._id LEFT JOIN Taxon ta ON t.accepted_id = ta._id LEFT JOIN TaxonDKnames dkn ON ta.vernacularname_dk_id = dkn._id LEFT JOIN Users dtm ON d.user_id = dtm._id LEFT JOIN ObservationImages oi ON oi.observation_id=o._id WHERE d.validation = 'Godkendt'
 GROUP BY o._id;
@@ -42,7 +43,7 @@ GROUP BY o._id;
 
 
 INTO outfile '/tmp/dwc.txt'
- FIELDS TERMINATED BY ','
+ FIELDS TERMINATED BY '\t'
  OPTIONALLY ENCLOSED BY '"'
  LINES TERMINATED BY '\n'
 
